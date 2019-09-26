0f047f2f8c0e53d0ae92b2038dbf3fc6ca3a8da4
Welber <jwwelber97@gmail.com>
2019-09-26 13:53:51 -0300

    title changed from A baleia to Domain Driven Design

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -8,6 +8,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+/*
+* this class will get created and managed by spring framework and the author and book repository
+* which we're going to get a implementation of from spring data JPA
+* those are going to get auto wired into here.
+ *
+* */
 @Component //to save the content
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,13 +35,19 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("A baleia", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
 
         //Rod
         Author rod = new Author("Rod","Austin");
         Book noEJB = new Book("J2EE Development without EJB","23444","Worx");
         rod.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
     }
 }
